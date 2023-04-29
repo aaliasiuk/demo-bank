c09acac67419ea9dee81a01041129eb114f6cb76
Andrei Aliasiuk <aliasiuk13@gmail.com>
2023-04-28 18:11:15 -0700

    add some code reformat

--- Account.java
+++ Account.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Account {
+import static java.lang.System.out;
+
+public class Account {
+    String name;
+    String address;
+    double balance;
+
+    public void display() {
+        out.print(name);
+        out.print(" (");
+        out.print(address);
+        out.print(") has on account $");
+        out.print(balance);
+    }
+
+    public double getInterest(double percentageRate) {
+        return balance * percentageRate / 100.00;
+    }
 }
